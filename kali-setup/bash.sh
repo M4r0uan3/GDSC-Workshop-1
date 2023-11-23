e305f7f5e7501c31dbadd27759436c8cb014225c
M4r0uan3 <marwane.berhili@gmail.com>
2023-11-23 01:26:28 +0100

    update setup.sh

--- kali-setup/bash.sh
+++ kali-setup/bash.sh
@@ -30,4 +30,26 @@ mkdir ~/Scripts
 echo "Downloading SecLists"
 git clone https://github.com/danielmiessler/SecLists ~/Scripts/SecLists
 
+# Install dependencies
+echo "Installing dependencies"
+sudo apt update
+sudo apt install -y \
+    ca-certificates
+    curl
+    gnupg
+    lsb-release
+
+echo "Adding Docker source"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+
+sudo add-apt-repository "https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+echo "Installing Docker Engine"
+sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+echo "Adding user to docker group"
+sudo gpasswd -a $USER docker
+
+echo "Docker is installed; log out and log in to run Docker without sudo!"
+
 exit 0
